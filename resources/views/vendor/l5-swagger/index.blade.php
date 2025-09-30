a7c9a953342070c8c23007cd9d011de05ef7902a
Lê Đăng Hoàng Tuấn <149415912+htuan154@users.noreply.github.com>
2025-09-30 21:40:48 +0700

    fix deloy 3

--- resources/views/vendor/l5-swagger/index.blade.php
+++ resources/views/vendor/l5-swagger/index.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <title>{{ config('l5-swagger.documentations.'.($documentation ?? 'default').'.api.title') }}</title>
-    <link rel="stylesheet" type="text/css" href="{{ l5_swagger_asset($documentation ?? 'default', 'swagger-ui.css') }}">
+    <!-- Use CDN for Swagger UI assets to avoid proxy/asset path issues in production -->
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/swagger-ui-dist@5.27.1/swagger-ui.css">
     <link rel="icon" type="image/png" href="{{ l5_swagger_asset($documentation ?? 'default', 'favicon-32x32.png') }}" sizes="32x32"/>
     <link rel="icon" type="image/png" href="{{ l5_swagger_asset($documentation ?? 'default', 'favicon-16x16.png') }}" sizes="16x16"/>
     <style>
@@ -119,8 +120,8 @@
 <body @if(config('l5-swagger.defaults.ui.display.dark_mode')) id="dark-mode" @endif>
 <div id="swagger-ui"></div>
 
-<script src="{{ l5_swagger_asset($documentation ?? 'default', 'swagger-ui-bundle.js') }}"></script>
-<script src="{{ l5_swagger_asset($documentation ?? 'default', 'swagger-ui-standalone-preset.js') }}"></script>
+<script src="https://cdn.jsdelivr.net/npm/swagger-ui-dist@5.27.1/swagger-ui-bundle.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/swagger-ui-dist@5.27.1/swagger-ui-standalone-preset.js"></script>
 <script>
     window.onload = function() {
         // Build a system
